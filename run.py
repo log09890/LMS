43d1ca0cdd89904d2c5026592b76504a89ed4347
Tung Han <114758494+log09890@users.noreply.github.com>
2026-04-15 02:50:54 +0700

    Change default port from 10000 to 500

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # Chỉ dùng khi chạy máy cá nhân
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 500))
     app.run(host='0.0.0.0', port=port)
