c97e3a406ab2043dfa8f4e33b1f63e75deb32915
Tung Han <hansontung2004@gmail.com>
2026-04-14 16:07:39 +0700

    1

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app
 
-# Khởi tạo toàn bộ ứng dụng
+# Gunicorn sẽ nhìn vào biến app này để chạy
 app = create_app()
 
 if __name__ == '__main__':
-    # Chạy Server
-    app.run(debug=True, port=5000)
+
+    app.run(debug=False)
